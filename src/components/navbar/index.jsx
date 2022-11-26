d62400bb9720384c36f4a0ce4df3f244b33e52a4
Parizzz26 <117721292+Parizzz26@users.noreply.github.com>
2022-11-26 21:48:31 +0700

    Add files via upload

--- src/components/navbar/index.jsx
+++ src/components/navbar/index.jsx
@@ -20,16 +20,6 @@ const Navbar = () => {
         </div>
       </div>
     </div>
-    // <div className="shadow shadow-kedua shadow-xl w-full fixed top-0 ring-kedua ring-black z-50">
-    //   <div className="text-2xl font-bold text-kedua justify-between bg-white py-4 md:px-10 px-7">
-    //     <div class="grid grid-flow-cols grid-rows-1 place-items-center">
-    //       <span><AiFillCaretLeft
-    //       type="rounded"
-    //       />List Of Transaction</span>
-    //     </div>
-     
-    //   </div>
-    // </div>
   );
   };
 
