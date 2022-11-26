172893dd4cf9c69937d8dc9dc7ef4efb33fc8ad0
Parizzz26 <117721292+Parizzz26@users.noreply.github.com>
2022-11-26 21:51:39 +0700

    Add files via upload

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,30 +1,12 @@
 import React from "react";
-//import { Banner, ProductCard } from "@components";
 import { useState } from "react";
 import { useEffect } from "react";
 import api from "../../services/api";
-import { Navbar, History, CardHistory } from "../../components";
-import { AiFillCaretLeft } from "react-icons/ai";
-//import { CaretLeftFilled } from "@ant-design/icons";
-//import { useNavigate } from "react-router-dom";
-//import { Button } from "antd";
+import { Navbar,  CardHistory } from "../../components";
+
 
 const HomePage = () => {
-  //const [cities, setCities] = useState([]);
   const [products, setProducts] = useState([]);
-  //  const navigate = useNavigate();
-
-  const fetchCities = async () => {
-    try {
-      const url = "/users";
-      const response = await api.get(url);
-      const payload = [...response?.data];
-      console.log(payload);
-      //  setCities(payload || []);
-    } catch (error) {
-      alert(error);
-    }
-  };
 
   const fetchProducts = async () => {
     try {
@@ -39,7 +21,6 @@ const HomePage = () => {
   };
 
   useEffect(() => {
-    fetchCities();
     fetchProducts();
   }, []);
 
