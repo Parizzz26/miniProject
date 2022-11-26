27730bcf84090014d4c2abcadbf08a6cfba64f05
Parizzz26 <117721292+Parizzz26@users.noreply.github.com>
2022-11-26 21:44:40 +0700

    Add files via upload

--- src/components/history/index.jsx
+++ src/components/history/index.jsx
@@ -44,59 +44,6 @@ const History = () => {
           </span>
         );
       })}
-
-      {/* <div className="pt-20 pb-10">
-
-      <div className="grid grid-cols-1 gap-y-10 place-items-center h-56">
-      {products.map((item) => {
-          return (
-            <div class="group block  mx-auto shadow-lg space-y-5 flex flex-col space-y-4 bg-white ring-2 ring-kedua rounded text-center w-1/2 hover:bg-utama hover:ring-kedua p-2">
-            <span key={item?.id}>
-            <div className="md:flex grid gap-x-8 gap-y-4 grid-cols-3  my-2 items-center justify-between md:px-10 px-7 text-slate-900 group-hover:text-white text-sm font-semibold" >
-            <div class="grid grid-rows-2 grid-flow-col gap-6">
-              <div class="row-span-2 my-8">
-                <AiFillCaretLeft
-                type="rounded"
-                />
-              </div>
-              <div class="col-span-1 ...">{item?.username}</div>
-              <div class="col-span-1 ...">Rp.{item?.id}</div>
-              <div class="row-span-2 my-8 object-right">{item?.name || "ga ada"}</div>
-            </div>
-              <div>
-                <div>
-                Rp.{item?.id}
-                </div>
-                <div>
-                {item?.username}
-                </div>
-                {item?.name || "ga ada"}
-              </div>
-              <div className="border-2 border-black">
-                <tr className="border-2 border-black">
-                  <td className="border-2 border-black">
-                  Rp.{item?.id}
-                  </td>
-                  <td className="border-2 border-black text-end" rowSpan={2}>
-                  {item?.name || "ga ada"}
-                  </td>
-                </tr> 
-                <tr className="border-2 border-black">
-                  <td >
-                  {item?.username}
-                  </td>
-                </tr>
-              </div>
-            </div>
-            </span>
-            </div>
-          );
-        })} 
-        <div class="group block mx-auto shadow-lg space-y-3 flex flex-col space-y-4 bg-white ring-2 ring-kedua rounded text-center w-5/6 hover:bg-utama hover:ring-kedua p-2">
-          <div className="text-slate-900 group-hover:text-white text-sm font-semibold">02</div>
-        </div>
-      </div>
-      </div> */}
     </div>
   );
 };
